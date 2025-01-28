52500dc51f4d37b31e1cd372a2e9fcb27d86e791
PaulT-cpu <ciprianptei@gmail.com>
2025-01-28 20:27:46 +0100

    Added the files product.py, product_manager.py, main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from product import Product
+from product_manager import ProductManager
+
+
+# Create an instance of the ProductManager class
+manage_products = ProductManager()
+
+
+# Add products to the product manager list
+product1 = Product("Sparring Gloves", 50, 10)
+product2 = Product("Boxing Shorts", 30, 20)
+product3 = Product("Headgear", 100, 5)
+
+manage_products.add_product(product1)
+manage_products.add_product(product2)
+manage_products.add_product(product3)
+
+
+# Display all products
+print("\nAll products:")
+manage_products.display_all_products()
+
+# Display the total value of all products
+print(f"\nTotal value of all products: ${manage_products.calculate_total_value()}")
